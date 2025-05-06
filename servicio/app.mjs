db5096f748f2990de3b01ad72d293a1dc23dea73
Sickospren <lopezllorentem@gmail.com>
2025-05-06 18:49:46 +0200

    token jwt añadido

--- servicio/app.mjs
+++ servicio/app.mjs
@@ -1,5 +1,4 @@
 import express from "express";
-import session from "express-session";
 import passport from "passport";
 import cors from "cors";
 import index_routes from "./routes/index.mjs";
@@ -9,28 +8,14 @@ const app = express()
 // Middlewares globales
 app.use(cors({
     origin: `http://localhost:5173`,
-    credentials:true
+    credentials: true
 }));
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-app.use(session({
-    secret: process.env.SECRET_APP,
-    resave: false,
-    saveUninitialized: false,
-    cookie:{
-        secure: false,
-        httpOnly: true,
-        maxAge: 86400000 //1Dia
-    }
-}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(passport.initialize());
-app.use(passport.session());
 
-// Rutas
-// establece un prefijo común, todas las rutas deben comenzar por /api
-app.use("/", index_routes) 
+app.use("/", index_routes)
 
 export default app
