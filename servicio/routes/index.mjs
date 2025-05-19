d4b0267e7d783c0797555fc33b404b21a118e29b
FNAFoxy2000 <marcosscr12@gmail.com>
2025-05-19 16:50:56 +0200

    router chats

--- servicio/routes/index.mjs
+++ servicio/routes/index.mjs
@@ -2,6 +2,7 @@ import express from "express"
 import router_usuario from "./router_usuario.mjs"
 import router_juego from "./router_juego.mjs"
 import router_auth from "./router_auth.mjs"
+import router_chat from "./router_chat.mjs";
 
 const router = express.Router()
 
@@ -26,6 +27,7 @@ router.get('/', (req, res) => {
 router.use("/usuario", router_usuario)
 router.use("/juegos", router_juego)
 router.use("/auth", router_auth)
+router.use("/chat", router_chat)
 
 export default router
 
---
+++ servicio/routes/router_chat.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import { getUserChats, getChatMessages } from "../controllers/chat_controller.mjs";
+
+const router = express.Router();
+
+// Obtener todos los chats de un usuario
+router.get("/usuario/:id_usuario", getUserChats);
+
+// Obtener todos los mensajes de un chat
+router.get("/:id_chat/mensajes", getChatMessages);
+
+export default router;
