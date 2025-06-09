3d34d77b75e527e483036501e260ad84aa2e8db4
Sickospren <lopezllorentem@gmail.com>
2025-06-09 16:47:47 +0200

    cosas que sobran

--- servicio/server.mjs
+++ servicio/server.mjs
@@ -2,10 +2,6 @@ import { app, server } from "./app.mjs";
 
 const port = 3000
 
-/* app.listen(port, () => {
-    console.log(`Servidor corriendo en localhost:${port}`)
-}) */
-
 // Iniciar el servidor
 server.listen(port, () => {
   console.log(`Servidor corriendo en localhost:${port}`);
