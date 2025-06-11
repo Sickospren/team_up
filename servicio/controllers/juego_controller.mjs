0671017489ca020a91d21f25d2efbb9300849031
FNAFoxy2000 <marcosscr12@gmail.com>
2025-06-11 18:42:34 +0200

    evitar que devuelva error cuando un juego no tiene comunidad

--- servicio/controllers/juego_controller.mjs
+++ servicio/controllers/juego_controller.mjs
@@ -1,34 +1,34 @@
-import { Juego, getAllJuegos ,getJuegoById, insertarJuego, actualizarJuego, eliminarJuego, getChatsJuegoById} from "../models/juego.mjs";
+import { Juego, getAllJuegos, getJuegoById, insertarJuego, actualizarJuego, eliminarJuego, getChatsJuegoById } from "../models/juego.mjs";
 
-export const getAllJuego = async(req,res) => {
-    try{        
+export const getAllJuego = async (req, res) => {
+    try {
         const juegos = await getAllJuegos();
 
-        if(!juegos){
-            res.status(404).json({mensaje:"Not found"});
+        if (!juegos) {
+            res.status(404).json({ mensaje: "Not found" });
             return
         }
 
         res.json(juegos);
-    }catch(error){
-        res.status(500).json({mensaje:"error al conseguir los datos de todos los juegos",error});
+    } catch (error) {
+        res.status(500).json({ mensaje: "error al conseguir los datos de todos los juegos", error });
     }
 
 }
 
-export const getJuegoID = async(req,res) => {
-    try{        
+export const getJuegoID = async (req, res) => {
+    try {
         const nombre_juego = req.params.nombre_juego;
         const juego = await getJuegoById(nombre_juego);
 
-        if(!juego){
-            res.status(404).json({mensaje:"Not found"});
+        if (!juego) {
+            res.status(404).json({ mensaje: "Not found" });
             return
         }
 
         res.json(juego);
-    }catch(error){
-        res.status(500).json({mensaje:"error al conseguir los datos del juego",error});
+    } catch (error) {
+        res.status(500).json({ mensaje: "error al conseguir los datos del juego", error });
     }
 
 }
@@ -67,7 +67,7 @@ export const annadirJuego = async (req, res) => {
 export const editarJuego = async (req, res) => {
     const juego = req.body;
 
-    if (!juego || !juego.nombre || !juego.descripcion || !juego.banner || !juego.foto_juego || 
+    if (!juego || !juego.nombre || !juego.descripcion || !juego.banner || !juego.foto_juego ||
         !juego.dispositivos || !juego.categoria || !juego.rangos) {
         return res.status(400).json({ mensaje: "Faltan datos para actualizar el juego" });
     }
@@ -105,7 +105,7 @@ export const editarJuego = async (req, res) => {
 
 export const borrarJuego = async (req, res) => {
     const juego = req.body;
-    if (!juego || !juego.nombre ) {
+    if (!juego || !juego.nombre) {
         return res.status(400).json({ mensaje: "Faltan datos para borrar el juego" });
     }
     try {
@@ -138,19 +138,21 @@ export const borrarJuego = async (req, res) => {
     }
 };
 
-export const getChatsJuego = async(req,res) => {
-    try{        
+export const getChatsJuego = async (req, res) => {
+    try {
         const id_juego = req.params.id_juego;
         const chats = await getChatsJuegoById(id_juego);
 
-        if(!chats){
-            res.status(404).json({mensaje:"Not found"});
-            return
+        if (!chats || chats.length === 0) {
+            return res.status(200).json({
+                mensaje: "Este juego no tiene ninguna comunidad",
+                chats: [],
+            });
         }
 
         res.json(chats);
-    }catch(error){
-        res.status(500).json({mensaje:"error al conseguir los chats del juego",error});
+    } catch (error) {
+        res.status(500).json({ mensaje: "error al conseguir los chats del juego", error });
     }
 
 }
